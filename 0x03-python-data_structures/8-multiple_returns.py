5dce2359e93bb9b6f1d58b4531a8a6190dceed2a
henry offor <henryoffor007@gmail.com>
2023-06-12 19:08:16 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    s_length = len(sentence)
+    if s_length == 0:
+        first_char = none
+    else:
+        first_char = sentence[0]
+    return((s_length, first_char))
