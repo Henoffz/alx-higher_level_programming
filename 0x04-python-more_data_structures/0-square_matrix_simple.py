a77b622613da95b0c27f08fc924263b223304b0d
henry offor <henryoffor007@gmail.com>
2023-06-14 15:40:17 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         new_row = []
         for j in range(len(matrix[i])):
-            new_row.append(matrix[i][j] **2)
+            new_row.append(matrix[i][j] ** 2)
         new_matrix.append(new_row[:])
     return new_matrix
