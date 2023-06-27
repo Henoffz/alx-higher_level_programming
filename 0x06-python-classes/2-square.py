19ef24f2a260db2184805a020e1361d83b7076fd
henry offor <henryoffor007@gmail.com>
2023-06-27 16:53:25 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Definition of Square class."""
+
+
+class Square:
+    """A class with private attributes."""
+
+    def __init__(self, size=0):
+        """Initializes private attributes of class.
+
+        Args:
+            size: square size.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
