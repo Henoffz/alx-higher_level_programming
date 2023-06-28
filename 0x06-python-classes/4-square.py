7148519bf3a58253c67d3515d19222c1548dc2e4
henry offor <henryoffor007@gmail.com>
2023-06-27 17:49:56 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -35,4 +35,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value                 
+        self.__size = value
