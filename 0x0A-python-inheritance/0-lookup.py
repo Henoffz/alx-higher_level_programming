f71cdea66e2a1ee6c8d1355227f528f9e72ed43b
henry offor <henryoffor007@gmail.com>
2023-07-10 10:31:12 -0700

    my commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that returns the list
+of available attributes and methods
+of an object.
+"""
+
+
+def lookup(obj):
+    """
+    function that returns
+    the list of objects
+    """
+    return (dir(obj))
