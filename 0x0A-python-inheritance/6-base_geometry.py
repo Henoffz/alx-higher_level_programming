fffe8f69e1cd92bb66367748bac899b1dbae81aa
henry offor <henryoffor007@gmail.com>
2023-07-10 17:59:12 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains the function: BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class: BaseGeometry"""
+    def area(self):
+        """instance of object"""
+        raise Exception("area() is not implemented")
