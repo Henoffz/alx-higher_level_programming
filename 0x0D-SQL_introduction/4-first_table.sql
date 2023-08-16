10b6e2357403d546f9949fa407b91d913d20d6ad
Henoffz <henryoffor007@gmail.com>
2023-08-16 02:06:48 +0000

    task 1-10

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Script that create a table in the
+-- current database of MySQL serber
+CREATE table IF NOT EXISTS first_table(id INT, name VARCHAR(256))
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- Script that create a table in the database in your MySQL
+-- server and add multiple rows.
+CREATE table IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+INSERT INTO second_table  VALUES (1, "John", 10), (2,  "Alex", 3), (3, "Bob", 14), (4, "George", 8);
