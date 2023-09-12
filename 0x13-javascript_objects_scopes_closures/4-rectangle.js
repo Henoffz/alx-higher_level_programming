1cc574df4d4e443ca5dc5a3ff89089afa6f68e0a
henry offor <henryoffor007@gmail.com>
2023-09-12 10:35:33 -0700

    commit update

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,26 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
-  }
-
-  print() {
-
-    for (let i = 0; i < this.height; i++) {
-      let row = "";
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-
-  rotate() {
-
-    [this.width, this.height] = [this.height, this.width];
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
-
-  double() {
-
-    this.width *= 2;
-    this.height *= 2;
+  rotate () {
+    let w = this.width;
+    let h = this.height;
+    this.height = w;
+    this.width = h;
+  }
+  double () {
+    this.height =* 2;
+    this.width =* 2;
   }
 };
