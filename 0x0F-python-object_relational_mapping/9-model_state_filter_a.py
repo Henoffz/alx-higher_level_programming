a78d0170639c078de4cdad7f48391492e7099159
Henoffz <henryoffor007@gmail.com>
2023-09-17 19:01:34 +0000

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 script to list all obj that contain letter a
-takes 3 args
+in the database.
 """
 
 from sys import argv
@@ -10,21 +10,17 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: ./script.py <username> <password> <database_name>")
-        exit()
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    state = session().query(State).filter(State.name.like('%a%')).order_by(
+    states = session.query(State).filter(State.name.like('%a%')).order_by(
         State.id).all()
 
     for state in states:
         if 'a' in state.name:
-            print(f"{state.id}: {state.name}")
+            print("{}: {}".format(state.id, state.name))
 
     session.close()
